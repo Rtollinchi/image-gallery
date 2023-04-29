645d5dcafeabc3b0db2a4d25e5bb6c29449dc120
Rtollinchi <rtollinchi23@gmail.com>
2023-04-29 14:02:33 -0400

    created new project image gallery

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["bit.ly","auqovpedpbblpnmsjojd.supabase.co"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
